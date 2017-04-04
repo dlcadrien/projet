026c5568333326454f1f8ad30f129c5a51dc8abd
dlc.adrien <dlc.adrien@gmail.com>
2017-04-04 10:29:59 -0400

    meteo 5

--- config/initializers/forecast.rb
+++ config/initializers/forecast.rb
@@ -1,4 +1,4 @@
 ForecastIO.configure do |configuration|
-  configuration.api_key = Rails.application.secrets.secret_forecast
   configuration.default_params = {units: 'si'}
+  configuration.api_key = Rails.application.secrets.secret_forecast
 end
